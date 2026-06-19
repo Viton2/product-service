f34092b45c80b4f91a55d07769712ceffcfba03b
Vitor <vitinhocastro2@gmail.com>
2026-06-19 19:36:34 -0300

    Clean project structure and add .gitignore

--- Model/Product.cs
+++ Model/Product.cs
@@ -1,12 +1,16 @@
-﻿namespace product_service_api.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace product_service_api.Model;
 
 public class Product
 {
     public Guid Id { get; set; }
 
+    [MaxLength(100)]
     public string Name { get; set; } = string.Empty;
-
+    [Range(typeof(decimal), "0.01", "999999.99", ErrorMessage = "Price must be between 0.01 and 999999.99")]
     public decimal Price { get; set; }
     
+    [MaxLength(100)]
     public string Description { get; set; } = string.Empty;
 }
